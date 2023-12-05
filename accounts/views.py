fe764f1d9e8c16f3a11478a801c45ad73059e624
SoroushGReza <mycodeschool22@outlook.com>
2023-12-05 15:16:01 +0000

    Added: AccountSerializer and listview

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Account
+from .serializers import AccountSerializer
 
 class AccountList(APIView):
     def get(self, request):
         accounts = Account.objects.all()
-        return Response(accounts)
+        serializer = AccountSerializer(
+            accounts, many=True,
+            context={'request': request}
+        )
+        return Response(serializer.data)
