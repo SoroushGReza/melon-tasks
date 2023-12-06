37c87880dbbc2e1a47de4c3a85ea2a2ad8f9659a
SoroushGReza <mycodeschool22@outlook.com>
2023-12-06 09:40:20 +0000

    Implemented: UserRegistrationSerializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,5 +1,37 @@
-from rest_framework import serializers
 from .models import Account
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+User = get_user_model()
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField(
+        style={'input_type': 'password'}, write_only=True
+    )
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'password2']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def save(self):
+        account = User(
+            email=self.validated_data['email'],
+            username=self.validated_data['username']
+        )
+        password = self.validated_data['password']
+        password2 = self.validated_data['password2']
+
+        if password != password2:
+            raise serializers.ValidationError(
+                {'password': 'Passwords must match.'}
+            )
+        account.set_password(password)
+        account.save()
+        return account
 
 
 class AccountSerializer(serializers.ModelSerializer):
