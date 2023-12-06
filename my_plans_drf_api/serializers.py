e4f3d71053e34161cd0aaf61b55c62a6ac1fb136
SoroushGReza <mycodeschool22@outlook.com>
2023-12-06 17:45:18 +0000

    Created: CurrentUserSerializer

---
+++ my_plans_drf_api/serializers.py
@@ -0,0 +1,11 @@
+from dj_rest_auth.serializers import UserDetailsSerializer
+from rest_framework import serializers
+
+class CurrentUserSerializer(UserDetailsSerializer):
+    account_id = serializers.ReadOnlyField(source='account.id')
+    account_image = serializers.ReadOnlyField(source='account.image.url')
+
+    class Meta(UserDetailsSerializer.Meta):
+        fields = UserDetailsSerializer.Meta.fields + (
+            'account_id', 'account_image'
+        )
