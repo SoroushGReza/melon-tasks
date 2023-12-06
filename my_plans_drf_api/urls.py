428956efb7075b5b0bbb96dfb97d46d370b044d6
SoroushGReza <mycodeschool22@outlook.com>
2023-12-06 17:44:41 +0000

    Added: registration endpoint

--- my_plans_drf_api/urls.py
+++ my_plans_drf_api/urls.py
@@ -21,6 +21,11 @@ import accounts.urls
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    path(
+        'dj-rest-auth/registration/',
+        include('dj_rest_auth.registration.urls')
+    ),
     path('', include('accounts.urls')),
     path('', include('tasks.urls')),
 ]
