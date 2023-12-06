e481b381897939a97afca3a0a7f9bf3edaa76877
SoroushGReza <mycodeschool22@outlook.com>
2023-12-06 09:37:39 +0000

    Added: UserRegistrationView and AccountDetail view

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,31 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Account
 from .serializers import AccountSerializer
+from .serializers import UserRegistrationSerializer
+from rest_framework.generics import GenericAPIView
+from my_plans_drf_api.permissions import IsOwnerOrReadOnly
+from django.contrib.auth import get_user_model
+from rest_framework import permissions, status
+from rest_framework.response import Response
+from rest_framework.generics import APIView, RetrieveUpdateDestroyAPIView
+
+
+User = get_user_model()
+
+class UserRegistrationView(GenericAPIView):
+    serializer_class = UserRegistrationSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class AccountList(APIView):
     def get(self, request):
@@ -11,3 +36,14 @@ class AccountList(APIView):
             context={'request': request}
         )
         return Response(serializer.data)
+
+
+class AccountDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
+    def get_object(self):
+        obj = super().get_object()
+        self.check_object_permissions(self.request, obj)
+        return obj
