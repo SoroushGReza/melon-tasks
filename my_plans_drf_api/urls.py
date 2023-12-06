f4bd33082bc2fc5cd9412a1c3627d40163fd202f
SoroushGReza <mycodeschool22@outlook.com>
2023-12-06 11:55:56 +0000

    Corrected: accounts url, Added: tasks url

--- my_plans_drf_api/urls.py
+++ my_plans_drf_api/urls.py
@@ -21,5 +21,6 @@ import accounts.urls
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('', include(accounts.urls)),
+    path('', include('accounts.urls')),
+    path('', include('tasks.urls')),
 ]
