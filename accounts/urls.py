bd05739914bb81e7f2f7ef1f01d32d5b6c56de3e
SoroushGReza <mycodeschool22@outlook.com>
2023-12-06 09:49:26 +0000

    Aded: url paths

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,18 @@
 from django.urls import path
 from accounts import views
+from .views import UserRegistrationView
 
 urlpatterns = [
     path('accounts/', views.AccountList.as_view()),
+    path('accounts/<int:pk>/', views.AccountDetail.as_view()),
+    path(
+        'account/<int:pk>/',
+        views.AccountDetail.as_view(),
+        name='account_detail',
+    ),
+    path(
+        'register/',
+        UserRegistrationView.as_view(),
+        name='register'
+    ),
 ]
