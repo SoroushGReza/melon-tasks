b23853207e139c8512c36a256e8a911c1fb08ca2
SoroushGReza <mycodeschool22@outlook.com>
2023-12-08 13:39:34 +0000

    Created: NavBar component and implemented React-bootstrap navbar

---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,37 @@
+import React from "react";
+import {
+  Navbar,
+  Container,
+  Nav,
+  NavDropdown,
+  Form,
+  FormControl,
+  Button,
+} from "react-bootstrap";
+
+const NavBar = () => {
+  return (
+    <Navbar bg="light" expand="md" fixed="top">
+      <Container>
+        <Navbar.Brand href="#home">My Plans</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Calender View" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Year</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Month</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
