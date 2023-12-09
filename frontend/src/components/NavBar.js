1d7b3633836220e81ac4e59181653e46aaf6ec4f
SoroushGReza <mycodeschool22@outlook.com>
2023-12-09 11:22:03 +0000

    Added: NavBar links with fontawesome icons

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -8,26 +8,40 @@ import {
   FormControl,
   Button,
 } from "react-bootstrap";
+import logo from "../assets/logo.png";
 
 const NavBar = () => {
   return (
     <Navbar bg="light" expand="md" fixed="top">
       <Container>
-        <Navbar.Brand href="#home">My Plans</Navbar.Brand>
+        <Navbar.Brand>
+          <img src={logo} alt="logo" height="45" />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Calender View" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Year</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Month</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
           <Form inline>
             <FormControl type="text" placeholder="Search" className="mr-sm-2" />
             <Button variant="outline-success">Search</Button>
           </Form>
+          <Nav className="ml-auto text-left">
+            <Nav.Link>
+              <i class="fa-solid fa-right-to-bracket"></i>Sign in
+            </Nav.Link>
+            <Nav.Link>
+              <i class="fa-solid fa-user-plus"></i>Sign up
+            </Nav.Link>
+            <NavDropdown
+              title={
+                <>
+                  <i class="fa-solid fa-calendar-days">{""}</i>View
+                </>
+              }
+              id="basic-nav-dropdown"
+            >
+              <NavDropdown.Item href="#action/3.1">Year</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Month</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
