d9bb587fa6e1c81389bf5cc83cbd74589d081fa8
SoroushGReza <mycodeschool22@outlook.com>
2023-12-09 14:23:26 +0000

    Updated: NavLink's

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -9,14 +9,18 @@ import {
   Button,
 } from "react-bootstrap";
 import logo from "../assets/logo.png";
+import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <Navbar bg="light" expand="md" fixed="top">
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
-        <Navbar.Brand>
-          <img src={logo} alt="logo" height="45" />
-        </Navbar.Brand>
+        <NavLink to="/">
+          <Navbar.Brand>
+            <img src={logo} alt="logo" height="45" />
+          </Navbar.Brand>
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Form inline>
@@ -24,12 +28,28 @@ const NavBar = () => {
             <Button variant="outline-success">Search</Button>
           </Form>
           <Nav className="ml-auto text-left">
-            <Nav.Link>
+            <NavLink
+              exact
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/"
+            >
+              <i class="fa-solid fa-house-chimney"></i>Home
+            </NavLink>
+            <NavLink
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/signin"
+            >
               <i className="fa-solid fa-right-to-bracket"></i>Sign in
-            </Nav.Link>
-            <Nav.Link>
+            </NavLink>
+            <NavLink
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/signup"
+            >
               <i className="fa-solid fa-user-plus"></i>Sign up
-            </Nav.Link>
+            </NavLink>
             <NavDropdown
               title={
                 <>
