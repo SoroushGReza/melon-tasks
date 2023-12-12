a76a6324ad2b55f31c8135beff21746bb1e018f1
SoroushGReza <mycodeschool22@outlook.com>
2023-12-12 18:36:40 +0000

    1. Added useContext Hook 2. Imported: User Context 3. Implemented User Context: Used setCurrentUser from SetCurrentUserContext
    Update SignIn Logic: Enhanced handleSubmit to set user context on successful sign-in.

--- frontend/src/pages/auth/SignInForm.js
+++ frontend/src/pages/auth/SignInForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import axios from "axios";
 
 import Form from "react-bootstrap/Form";
@@ -14,8 +14,11 @@ import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
 import DidUKnowImage from "../../assets/diduknow.jpg";
+import { SetCurrentUserContext } from "../../App";
 
 const SignInForm = () => {
+  const setCurrentUser = useContext(SetCurrentUserContext);
+
   const [signInData, setSignInData] = useState({
     username: "",
     password: "",
@@ -28,7 +31,8 @@ const SignInForm = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      await axios.post("/dj-rest-auth/login/", signInData);
+      const { data } = await axios.post("/dj-rest-auth/login/", signInData);
+      setCurrentUser(data.user);
       history.push("/");
     } catch (err) {
       setErrors(err.response.data);
