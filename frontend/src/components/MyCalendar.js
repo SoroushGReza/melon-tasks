301660f24e495e10187661b7246eca9462cfece9
SoroushGReza <mycodeschool22@outlook.com>
2023-12-16 12:08:16 +0000

    Replaced: react-calendar with react-fullcalendar (No styling added)

--- frontend/src/components/MyCalendar.js
+++ frontend/src/components/MyCalendar.js
@@ -1,22 +1,15 @@
-import React, { useState } from "react";
-import Calendar from "react-calendar";
-import "react-calendar/dist/Calendar.css";
-import Container from "react-bootstrap/Container";
-import styles from "../styles/MyCalendar.module.css";
+import React from 'react';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import Container from 'react-bootstrap/Container';
+import styles from '../styles/MyCalendar.module.css';
 
 const MyCalendar = () => {
-  const [date, setDate] = useState(new Date());
-
-  const onChange = (newDate) => {
-    setDate(newDate);
-  };
-
   return (
     <Container className={`p-4 ${styles.calendarContainer}`}>
-      <Calendar
-        onChange={onChange}
-        value={date}
-        className={styles.reactCalendar}
+      <FullCalendar
+        plugins={[dayGridPlugin]}
+        initialView="dayGridMonth"
       />
     </Container>
   );
