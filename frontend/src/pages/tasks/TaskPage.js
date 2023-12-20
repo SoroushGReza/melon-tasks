8ec5283b791da93588755357176a39dde27e2227
SoroushGReza <mycodeschool22@outlook.com>
2023-12-20 15:36:59 +0000

    Imported and added: "Task" component

--- frontend/src/pages/tasks/TaskPage.js
+++ frontend/src/pages/tasks/TaskPage.js
@@ -6,6 +6,7 @@ import Container from "react-bootstrap/Container";
 import appStyles from "../../App.module.css";
 import { useParams } from "react-router-dom";
 import { axiosReq } from "../../api/axiosDefaults";
+import Task from "./Task";
 
 function TaskPage() {
   const { id } = useParams();
@@ -31,7 +32,7 @@ function TaskPage() {
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles for mobile</p>
-        <p>Task component</p>
+        <Task {...task.results[0]} setTasks={setTask} />
         <Container className={appStyles.Content}>Comments</Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
