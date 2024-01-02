2621cc0476bf8186d50ed21416b4bc3147b24c70
SoroushGReza <mycodeschool22@outlook.com>
2024-01-02 13:16:09 +0000

    Added: feature to enable MyCalendar to display tasks

--- frontend/src/components/MyCalendar.js
+++ frontend/src/components/MyCalendar.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import Container from 'react-bootstrap/Container';
 import styles from '../styles/MyCalendar.module.css';
 
-const MyCalendar = () => {
+const MyCalendar = ({ tasks }) => {
+  const [calendarTasks, setCalendarTasks] = useState([]);
+
+  useEffect(() => {
+    const calendarTasksPreview = tasks.map(task => ({
+      title: task.title,
+      date: task.due_date
+    }));
+    setCalendarTasks(calendarTasksPreview);
+  }, [tasks]);
+
   return (
     <Container className={`p-4 ${styles.calendarContainer}`}>
       <FullCalendar
         plugins={[dayGridPlugin]}
         initialView="dayGridMonth"
+        events={calendarTasks}
       />
     </Container>
   );
