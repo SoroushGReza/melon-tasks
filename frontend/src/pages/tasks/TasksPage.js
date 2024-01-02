a7b91092ff96ed0b1fa5c268ecafa686a8788c02
SoroushGReza <mycodeschool22@outlook.com>
2024-01-02 21:58:46 +0000

    Added: search functionality to the tasks page to search tasks by query

--- frontend/src/pages/tasks/TasksPage.js
+++ frontend/src/pages/tasks/TasksPage.js
@@ -18,10 +18,12 @@ function TasksPage({ message, filter = "" }) {
   const [hasLoaded, setHasLoaded] = useState(false);
   const { pathname } = useLocation();
 
+  const [query, setQuery] = useState("");
+
   useEffect(() => {
     const fetchTasks = async () => {
       try {
-        const { data } = await axiosReq.get(`/tasks/?$(filter)`);
+        const { data } = await axiosReq.get(`/tasks/?${filter}search=${query}`);
         setTasks(data);
         setHasLoaded(true);
       } catch (err) {
@@ -29,12 +31,31 @@ function TasksPage({ message, filter = "" }) {
       }
     };
     setHasLoaded(false);
+    const timer = setTimeout(() => {
+      fetchTasks();
+    }, 1000);
+    return () => {
+      clearTimeout(timer);
+    };
     fetchTasks();
-  }, [filter, pathname]);
+  }, [filter, query, pathname]);
 
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
+        <i className={`fa-solid fa-magnifying-glass ${styles.SearchIcon}`} />
+        <Form
+          className={styles.SearchBar}
+          onSubmit={(event) => event.preventDefault()}
+        >
+          <Form.Control
+            value={query}
+            onChange={(event) => setQuery(event.target.value)}
+            type="text"
+            className="mr-sm-2"
+            placeholder="Search tasks"
+          />
+        </Form>
         {hasLoaded ? (
           <>
             {tasks.results.length ? (
