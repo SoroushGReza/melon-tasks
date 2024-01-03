dc033582235a026b828b937a01f076a24cd2cac1
SoroushGReza <mycodeschool22@outlook.com>
2024-01-03 14:52:44 +0000

    1. Added: removeTaskFromState function to TaskPage for state management 2. Pass removeTask prop to Task component for delete update

--- frontend/src/pages/tasks/TaskPage.js
+++ frontend/src/pages/tasks/TaskPage.js
@@ -12,6 +12,10 @@ function TaskPage() {
   const { id } = useParams();
   const [task, setTask] = useState({ results: [] });
 
+  const removeTaskFromState = () => {
+    setTask({ results: [] }); // Clear task from state
+  };
+
   useEffect(() => {
     const handleMount = async () => {
       try {
@@ -32,7 +36,12 @@ function TaskPage() {
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles for mobile</p>
-        <Task {...task.results[0]} setTasks={setTask} taskPage />
+        <Task
+          {...task.results[0]}
+          setTasks={setTask}
+          taskPage
+          removeTask={removeTaskFromState}
+        />
         <Container className={appStyles.Content}>Comments</Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
