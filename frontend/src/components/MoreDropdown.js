79a0db013eae16d8ea8df767d748332ebdf9303d
SoroushGReza <mycodeschool22@outlook.com>
2024-01-03 18:00:37 +0000

    Added: styling to MoreDropdown

--- frontend/src/components/MoreDropdown.js
+++ frontend/src/components/MoreDropdown.js
@@ -19,7 +19,7 @@ export const MoreDropdown = ({ handleEdit, handleDelete }) => {
       <Dropdown.Toggle as={ArrowDown} />
 
       <Dropdown.Menu
-        className="text-center"
+        className={`text-center ${styles.CustomDropdownMenu}`}
         popperConfig={{ strategy: "fixed" }}
       >
         <Dropdown.Item
@@ -27,14 +27,16 @@ export const MoreDropdown = ({ handleEdit, handleDelete }) => {
           onClick={handleEdit}
           aria-label="edit"
         >
-          <i className="fa-solid fa-pen-to-square" />
+          <i
+            className={`fa-solid fa-pen-to-square ${styles.MoreDropdownIcon}`}
+          />{" "}
         </Dropdown.Item>
         <Dropdown.Item
           className={styles.DropdownItem}
           onClick={handleDelete}
           aria-label="delete"
         >
-          <i className="fas fa-trash-alt" />
+          <i className={`fas fa-trash-alt ${styles.MoreDropdownIcon}`} />{" "}
         </Dropdown.Item>
       </Dropdown.Menu>
     </Dropdown>
