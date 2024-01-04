3a4227925abffe4c44a72f3aca03a532a8ca06aa
SoroushGReza <mycodeschool22@outlook.com>
2024-01-04 19:35:51 +0000

    Updated: token refresh URL to absolute path to fix refresh token error (SUCCESS)

--- frontend/src/contexts/CurrentUserContext.js
+++ frontend/src/contexts/CurrentUserContext.js
@@ -1,4 +1,11 @@
-import { createContext, useContext, useEffect, useMemo, useState, useCallback } from "react";
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useMemo,
+  useState,
+  useCallback,
+} from "react";
 import axios from "axios";
 import { axiosReq, axiosRes } from "../api/axiosDefaults";
 import { useHistory } from "react-router-dom";
@@ -22,6 +29,7 @@ export const CurrentUserProvider = ({ children }) => {
       console.log(err);
     }
   }, [history]);
+
   useEffect(() => {
     handleMount();
   }, [handleMount]);
@@ -30,7 +38,9 @@ export const CurrentUserProvider = ({ children }) => {
     axiosReq.interceptors.request.use(
       async (config) => {
         try {
-          await axios.post("dj-rest-auth/token/refresh/");
+          await axios.post(
+            `${window.location.origin}/dj-rest-auth/token/refresh/`
+          );
         } catch (err) {
           setCurrentUser((prevCurrentUser) => {
             if (prevCurrentUser) {
@@ -52,7 +62,9 @@ export const CurrentUserProvider = ({ children }) => {
       async (err) => {
         if (err.response?.status === 401) {
           try {
-            await axios.post("dj-rest-auth/token/refresh/");
+            await axios.post(
+              `${window.location.origin}/dj-rest-auth/token/refresh/`
+            );
           } catch (err) {
             setCurrentUser((prevCurrentUser) => {
               if (prevCurrentUser) {
