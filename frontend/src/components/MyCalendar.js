81a63bf5b219a5c2dbb5c11cecd61d89a9c103bc
SoroushGReza <mycodeschool22@outlook.com>
2024-01-05 15:53:40 +0000

    1. Removed: form and edit functionality from modal. 2. Removed: unused state and handler functions. 3. Implemented: redirection to edit page using react-router-dom. 4. Removed: unused imports and clean up code. 5. Changed: modal to display task details only. 6. Updated: modal title and button to reflect view-only details. 7. Removed: image handling from state. 8. Organized & optimized: import statements.

--- frontend/src/components/MyCalendar.js
+++ frontend/src/components/MyCalendar.js
@@ -2,25 +2,15 @@ import React, { useEffect, useState } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import Container from "react-bootstrap/Container";
-import { Modal, Button, Form, Image } from "react-bootstrap";
+import { Modal, Button, Image } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
 import styles from "../styles/MyCalendar.module.css";
-import { axiosReq } from "../api/axiosDefaults";
 
 const MyCalendar = ({ tasks }) => {
   const [calendarTasks, setCalendarTasks] = useState([]);
   const [showModal, setShowModal] = useState(false);
   const [selectedTask, setSelectedTask] = useState(null);
-  const [taskData, setTaskData] = useState({
-    title: "",
-    content: "",
-    image: "",
-    due_date: "",
-    priority: "medium",
-    category: "other",
-    status: "open",
-    is_overdue: false,
-    is_public: false,
-  });
+  const history = useHistory();
 
   useEffect(() => {
     const calendarTasksPreview = tasks.map((task) => ({
@@ -36,86 +26,14 @@ const MyCalendar = ({ tasks }) => {
     const task = tasks.find((task) => task.id.toString() === taskId);
     if (task) {
       setSelectedTask(task);
-      setTaskData({ ...task });
       setShowModal(true);
     } else {
       console.error("Task not found for ID:", taskId);
     }
   };
 
-  const handleChange = (event) => {
-    const { name, value, type, checked, files } = event.target;
-    if (name === "image" && files.length) {
-      setTaskData({
-        ...taskData,
-        image: files[0],
-      });
-    } else {
-      setTaskData({
-        ...taskData,
-        [name]: type === "checkbox" ? checked : value,
-      });
-    }
-  };
-
-  const handleSaveChanges = async (event) => {
-    event.preventDefault();
-    if (selectedTask && selectedTask.id) {
-      try {
-        const formData = new FormData();
-        Object.keys(taskData).forEach((key) => {
-          // Append only if the field has a value and it's not an empty string
-          // Exclude 'image' and 'permits_users' from this general rule
-          if (
-            taskData[key] !== "" &&
-            key !== "image" &&
-            key !== "permit_users"
-          ) {
-            formData.append(key, taskData[key]);
-          }
-          if (key === "image" && taskData[key] instanceof File) {
-            formData.append(key, taskData[key]);
-          }
-          // Include 'permit_users' only if it's explicitly set and is a non-empty array
-          if (
-            key === "permit_users" &&
-            Array.isArray(taskData[key]) &&
-            taskData[key].length > 0
-          ) {
-            // Append each username in the 'permit_users' array
-            taskData[key].forEach((username) => {
-              formData.append("permit_users", username);
-            });
-          }
-        });
-        await axiosReq.put(`/tasks/${selectedTask.id}/`, formData, {
-          headers: {
-            "Content-Type": "multipart/form-data",
-          },
-        });
-        setShowModal(false);
-      } catch (err) {
-        console.log("Error:", err);
-        if (err.response && err.response.data) {
-          console.log("Error data:", err.response.data);
-        }
-      }
-    } else {
-      console.error("No task selected or task has no ID!");
-    }
-  };
-
-  const handleDeleteTask = async () => {
-    if (selectedTask && selectedTask.id) {
-      try {
-        await axiosReq.delete(`/tasks/${selectedTask.id}/`);
-        setShowModal(false);
-      } catch (err) {
-        console.log(err);
-      }
-    } else {
-      console.error("No task selected or task has no ID!");
-    }
+  const handleEditRedirect = () => {
+    history.push(`/tasks/${selectedTask.id}/edit`);
   };
 
   return (
@@ -129,66 +47,56 @@ const MyCalendar = ({ tasks }) => {
 
       <Modal show={showModal} onHide={() => setShowModal(false)}>
         <Modal.Header closeButton>
-          <Modal.Title>Edit Task</Modal.Title>
+          <Modal.Title>Task Details</Modal.Title>
         </Modal.Header>
-        <Form onSubmit={handleSaveChanges}>
-          <Modal.Body>
-            <Form.Group>
-              <Form.Label>Title</Form.Label>
-              <Form.Control
-                type="text"
-                name="title"
-                value={taskData.title || ""}
-                onChange={handleChange}
-              />
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>Content</Form.Label>
-              <Form.Control
-                as="textarea"
-                rows={3}
-                name="content"
-                value={taskData.content || ""}
-                onChange={handleChange}
-              />
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>Due Date</Form.Label>
-              <Form.Control
-                type="date"
-                name="due_date"
-                value={taskData.due_date || ""}
-                onChange={handleChange}
-              />
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>Image</Form.Label>
-              <Form.Control type="file" name="image" onChange={handleChange} />
-              {taskData.image && (
-                <Image
-                  src={
-                    typeof taskData.image === "string"
-                      ? taskData.image
-                      : URL.createObjectURL(taskData.image)
-                  }
-                  alt="Task"
-                  fluid
-                />
+        <Modal.Body>
+          {selectedTask && (
+            <>
+              <p>
+                <strong>Title:</strong> {selectedTask.title}
+              </p>
+              <p>
+                <strong>Content:</strong> {selectedTask.content}
+              </p>
+              <p>
+                <strong>Due Date:</strong> {selectedTask.due_date}
+              </p>
+              <p>
+                <strong>Priority:</strong> {selectedTask.priority}
+              </p>
+              <p>
+                <strong>Category:</strong> {selectedTask.category}
+              </p>
+              <p>
+                <strong>Status:</strong> {selectedTask.status}
+              </p>
+              <p>
+                <strong>Is Overdue:</strong>{" "}
+                {selectedTask.is_overdue ? "Yes" : "No"}
+              </p>
+              <p>
+                <strong>Is Public:</strong>{" "}
+                {selectedTask.is_public ? "Yes" : "No"}
+              </p>
+              {selectedTask.image && (
+                <div>
+                  <p>
+                    <strong>Image:</strong>
+                  </p>
+                  <Image src={selectedTask.image} alt="Task" fluid />
+                </div>
               )}
-            </Form.Group>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={() => setShowModal(false)}>
-              Close
-            </Button>
-            <Button variant="danger" onClick={handleDeleteTask}>
-              Delete Task
-            </Button>
-            <Button variant="primary" type="submit">
-              Save Changes
-            </Button>
-          </Modal.Footer>
-        </Form>
+            </>
+          )}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setShowModal(false)}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleEditRedirect}>
+            Edit Task
+          </Button>
+        </Modal.Footer>
       </Modal>
     </Container>
   );
