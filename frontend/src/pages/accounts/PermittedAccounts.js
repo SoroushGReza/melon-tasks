f2ba1035672a978e7c5f1b0aa8c1bdcee3c5211f
SoroushGReza <mycodeschool22@outlook.com>
2024-01-06 14:19:18 +0000

    1. Refactored: Replace local state with useAccountData context for permitted accounts. 2. Refactored: Simplify conditional rendering for mobile styling. 3. Refactored: Remove unused imports and clean up code.

--- frontend/src/pages/accounts/PermittedAccounts.js
+++ frontend/src/pages/accounts/PermittedAccounts.js
@@ -1,59 +1,32 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import styles from "../../styles/PermittedAccounts.module.css";
 import { Container } from "react-bootstrap";
-import { axiosReq } from "../../api/axiosDefaults";
-import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { useAccountData } from "../../contexts/AccountDataContext";
 import Asset from "../../components/Asset";
 import Account from "./Account";
 
 const PermittedAccounts = ({ mobile }) => {
-  const [accountData, setAccountData] = useState({
-    pageAccount: { results: [] },
-    permittedAccounts: { results: [] },
-  });
-  const { permittedAccounts } = accountData;
-  const currentUser = useCurrentUser();
-
-  useEffect(() => {
-    const handleMount = async () => {
-      try {
-        const { data } = await axiosReq.get(
-          "/accounts/?ordering=-permit_users"
-        );
-        setAccountData((prevState) => ({
-          ...prevState,
-          permittedAccounts: data,
-        }));
-      } catch (err) {
-        console.log(err);
-      }
-    };
-
-    handleMount();
-  }, [currentUser]);
+  const { permittedAccounts } = useAccountData();
 
   return (
     <Container
       className={`${styles.PermittedAccounts} ${
-        mobile && "d-lg-none text-center mb-3"
+        mobile ? "d-lg-none text-center mb-3" : ""
       }`}
     >
       {permittedAccounts.results.length ? (
         <>
           <p>Permitted users</p>
-          {mobile ? (
-            <div
-              className={`${styles.PermittedMobile} d-flex justify-content-center`}
-            >
-              {permittedAccounts.results.slice(0, 4).map((account) => (
-                <Account key={account.id} account={account} mobile />
-              ))}
-            </div>
-          ) : (
-            permittedAccounts.results.map((account) => (
-              <Account key={account.id} account={account} />
-            ))
-          )}
+          <div className={`${mobile ? styles.PermittedMobile : ""}`}>
+            {permittedAccounts.results.map((account) => (
+              <div
+                key={account.id}
+                className={mobile ? "d-flex justify-content-center" : ""}
+              >
+                <Account account={account} mobile={mobile} />
+              </div>
+            ))}
+          </div>
         </>
       ) : (
         <Asset spinner />
