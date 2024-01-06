b69ad2413821f863edc9c1937b0e22b36a02448b
SoroushGReza <mycodeschool22@outlook.com>
2024-01-06 16:34:37 +0000

    Addded: Route AccountPage component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,7 @@ import TaskPage from "./pages/tasks/TaskPage";
 import TasksPage from "./pages/tasks/TasksPage";
 import { useCurrentUser } from "./contexts/CurrentUserContext";
 import TaskEditForm from "./pages/tasks/TaskEditForm";
+import AccountPage from "./pages/accounts/AccountPage";
 
 function App() {
   const currentUser = useCurrentUser();
@@ -53,6 +54,7 @@ function App() {
           <Route exact path="/tasks/:id" render={() => <TaskPage />} />
           <Route exact path="/tasks" render={() => <TasksPage />} />
           <Route exact path="/tasks/:id/edit" render={() => <TaskEditForm />} />
+          <Route exact path="/accounts/:id" render={() => <AccountPage />} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
