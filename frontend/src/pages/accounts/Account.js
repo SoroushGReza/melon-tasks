efb8ec23fa5be239443dead33d5284308d7f8a52
SoroushGReza <mycodeschool22@outlook.com>
2024-01-06 11:40:57 +0000

    1. Created: Account component with props and context. 2. Rendered: user account details including avatar, name, and authorization buttons. 3. Adjusted: layout and styling based on mobile prop. 4. Added: conditional rendering for authorization buttons based on user role.

---
+++ frontend/src/pages/accounts/Account.js
@@ -0,0 +1,51 @@
+import React from "react";
+import styles from "../../styles/Account.module.css";
+import btnStyles from "../../styles/Button.module.css";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { Link } from "react-router-dom";
+import Avatar from "../../components/Avatar";
+import { Button } from "react-bootstrap";
+
+const Account = (props) => {
+  const { account, mobile, imageSize = 45 } = props;
+  const { id, permit_users, image, owner } = account;
+
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
+  return (
+    <div
+      className={`my-3 d-flex align-items-center ${mobile && "flex-column"}`}
+    >
+      <div>
+        <Link className="align-self-center" to={`/accounts/${id}`}>
+          <Avatar src={image} height={imageSize} />
+        </Link>
+      </div>
+      <div className={`mx-2 ${styles.WordBreak}`}>
+        <strong>{owner}</strong>
+      </div>
+      <div className={`text-right ${!mobile && "ml-auto"}`}>
+        {!mobile &&
+          currentUser &&
+          !is_owner &&
+          (permit_users ? (
+            <Button
+              className={`${btnStyles.Button} ${btnStyles.BlackOutline}`}
+              onClick={() => {}}
+            >
+              Unauthorize
+            </Button>
+          ) : (
+            <Button
+              className={`${btnStyles.Button} ${btnStyles.Black}`}
+              onClick={() => {}}
+            >
+              Authorize
+            </Button>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default Account;
