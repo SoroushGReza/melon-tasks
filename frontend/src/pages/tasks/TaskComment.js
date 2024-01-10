59971f8f3e02eb5914a228e41e81212d46ff82c2
SoroushGReza <mycodeschool22@outlook.com>
2024-01-10 17:06:41 +0000

    Created TaskComment component: - Imported: necessary React components and styles for TaskComment component. - Created: TaskComment functional component with props for task comment details. - Implemented: current user context to check if user is author of comment. - Rendered: task comment with a link to the user's account, avatar, author, content, and created_at .

---
+++ frontend/src/pages/tasks/TaskComment.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Media } from "react-bootstrap";
+import Avatar from "../../components/Avatar";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import styles from "../../styles/TaskComment.module.css";
+import { Link } from "react-router-dom";
+
+const TaskComment = (props) => {
+  const { account_id, account_image, owner, created_at, content, author } =
+    props;
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === author;
+
+  return (
+    <div>
+      <hr />
+      <Media as="li" className="mt-3">
+        <Link to={`/accounts/${account_id}`}>
+          <Avatar src={currentUser?.account_image} height={20} />
+        </Link>
+        <Media.Body>
+          <h6 className={styles.Owner}>{author}</h6>
+          <p>{content}</p>
+          <small className={styles.Date}>{created_at}</small>
+        </Media.Body>
+      </Media>
+    </div>
+  );
+};
+
+export default TaskComment;
