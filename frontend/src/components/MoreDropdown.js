027a119c8aa96882a60863252f0280ebd2888e22
SoroushGReza <mycodeschool22@outlook.com>
2024-01-12 17:10:31 +0000

    1. Added: AccountEditDropdown for managing account setings. 2. Used: useHistory from React Router to navigate to edit pages. 3. Added: options in the dropdown for managing account, editing username and password.    (Each option navigates to a specific edit page when clicked.)

--- frontend/src/components/MoreDropdown.js
+++ frontend/src/components/MoreDropdown.js
@@ -1,6 +1,42 @@
 import React from "react";
 import Dropdown from "react-bootstrap/Dropdown";
 import styles from "../styles/MoreDropdown.module.css";
+import { useHistory } from "react-router-dom";
+
+// Dropdown for editing user Account
+
+export function AccountEditDropdown({ id }) {
+  const history = useHistory();
+  return (
+    <Dropdown className={`ml-auto px-3 ${styles.Absolute}`} drop="left">
+      <Dropdown.Toggle as={DotsDropDown} />
+      <Dropdown.Menu>
+        <Dropdown.Item
+          onClick={() => history.push(`/accounts/${id}/edit`)}
+          aria-label="manage-account"
+        >
+          <i className="fas fa-edit" /> manage account
+        </Dropdown.Item>
+        <Dropdown.Item
+          onClick={() => history.push(`/accounts/${id}/edit/username`)}
+          aria-label="edit-username"
+        >
+          <i className="far fa-id-card" />
+          change username
+        </Dropdown.Item>
+        <Dropdown.Item
+          onClick={() => history.push(`/accounts/${id}/edit/password`)}
+          aria-label="edit-password"
+        >
+          <i className="fas fa-key" />
+          change password
+        </Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+}
+
+// Dropdown for Edit/Delete Tasks and Comments
 
 const DotsDropDown = React.forwardRef(({ onClick }, ref) => (
   <i
