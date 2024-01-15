d272ac0ba9d8d1af24fecc14449adb8315ee2d88
SoroushGReza <mycodeschool22@outlook.com>
2024-01-15 16:51:47 +0000

    Added: path for "delete-user/"

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from accounts import views
-from .views import UserRegistrationView
+from .views import UserRegistrationView, DeleteUserView
 
 urlpatterns = [
     path('accounts/', views.AccountList.as_view()),
@@ -13,6 +13,11 @@ urlpatterns = [
     path(
         'register/',
         UserRegistrationView.as_view(),
-        name='register'
+        name='register',
+    ),
+    path(
+        'delete-user/',
+        DeleteUserView.as_view(),
+        name='delete-user'
     ),
 ]
