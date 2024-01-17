5d0448c11860b365b355a693a06716089b28421e
SoroushGReza <mycodeschool22@outlook.com>
2024-01-17 12:04:46 +0000

    1. Added: responsiveness to the NewestAccounts component 2. Limited: accounts to be shown to 5 in large screens,  and 3 on smaller screens. 3. Gave the component some styling 4. Changed: Name of the component to match its purpose.

---
+++ frontend/src/pages/accounts/NewestAccounts.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from "react";
+import styles from "../../styles/NewestAccounts.module.css";
+import { Container, Col, Row } from "react-bootstrap";
+import { useAccountData } from "../../contexts/AccountDataContext";
+import Asset from "../../components/Asset";
+import Account from "./Account";
+
+const NewestAccounts = ({ mobile }) => {
+  const { newestAccounts } = useAccountData();
+  const [displayedAccounts, setDisplayedAccounts] = useState([]);
+
+  useEffect(() => {
+    if (newestAccounts.results) {
+      // Sort by created_at in descending order
+      const sortedAccounts = [...newestAccounts.results].sort(
+        (a, b) =>
+          new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
+      );
+      console.log("Sorted accounts:", sortedAccounts); // DEBUGGING
+
+      // Determine nr of accounts to display based on screen size
+      const numAccountsToShow = mobile ? 3 : 5;
+      setDisplayedAccounts(sortedAccounts.slice(0, numAccountsToShow));
+    }
+  }, [newestAccounts, mobile]);
+
+  // Adjusted column sizes for different screen sizes
+  const columnSize = mobile ? 4 : 12;
+
+  return (
+    <Container
+      className={`${styles.NewestAccounts} ${
+        mobile ? "d-lg-none text-center mb-3" : ""
+      }`}
+    >
+      {displayedAccounts.length ? (
+        <>
+          <p className={styles.NewestUsersP}>Newest users</p>
+          <Row className={`${mobile ? styles.NewestAccountsMobile : ""}`}>
+            {displayedAccounts.map((account) => (
+              <Col key={account.id} xs={columnSize} className="mb-3">
+                <Account account={account} mobile={mobile} />
+              </Col>
+            ))}
+          </Row>
+        </>
+      ) : (
+        <Asset spinner />
+      )}
+    </Container>
+  );
+};
+
+export default NewestAccounts;
