5d81ccf39d270b68a98ef51c8656e20c5aec4d57
SoroushGReza <mycodeschool22@outlook.com>
2024-01-17 18:02:47 +0000

    1. Imported: useRef from React. 2. Renamed: 'expanded' to 'navbarExpanded' and added 'searchExpanded' for better manage. 3. Added: 'useClickOutsideToggle' with useRef to close search results on outside click. 4. Updated: 'handleSearchChange' to set 'searchExpanded' true when displaying search results. 5. Added: 'closeSearchResults' function to hide search results when user is selected. 6. Prevented: default form submission behaviuor in search form. 7. Displayed: 'No user found' text when no search results are available. 8. Added: onClick event to search results for closing dropdown on user selection. 9. Removed: unused Button

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,11 +1,19 @@
-import React, { useState } from "react";
-import { Form, FormControl, Button } from "react-bootstrap";
-import { Navbar, Container, Nav } from "react-bootstrap";
+import React, { useState, useRef } from "react";
+import {
+  Form,
+  FormControl,
+  Button,
+  Navbar,
+  Container,
+  Nav,
+} from "react-bootstrap";
 import logo from "../assets/logo.png";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
-import { useSetCurrentUser } from "../contexts/CurrentUserContext";
-import { useCurrentUser } from "../contexts/CurrentUserContext";
+import {
+  useSetCurrentUser,
+  useCurrentUser,
+} from "../contexts/CurrentUserContext";
 import Avatar from "./Avatar";
 import axios from "axios";
 import { axiosReq } from "../api/axiosDefaults";
@@ -15,32 +23,37 @@ const NavBar = () => {
   const currentUser = useCurrentUser();
   const setCurrentUser = useSetCurrentUser();
 
-  const { expanded, setExpanded, ref } = useClickOutsideToggle();
+  const navbarRef = useRef(null);
+  const searchRef = useRef(null);
 
+  const [navbarExpanded, setNavbarExpanded] = useState(false);
+  const [searchExpanded, setSearchExpanded] = useState(false);
   const [searchQuery, setSearchQuery] = useState("");
   const [searchResults, setSearchResults] = useState([]);
+
+  useClickOutsideToggle(() => setNavbarExpanded(false), navbarRef);
+  useClickOutsideToggle(() => setSearchExpanded(false), searchRef);
 
   const handleSearchChange = async (e) => {
     const query = e.target.value;
     setSearchQuery(query);
 
     if (query.length === 0) {
-      setSearchResults([]); // Clear results when input is empty
+      setSearchResults([]);
     } else {
       try {
         const { data } = await axiosReq.get(`/search-users/?username=${query}`);
         setSearchResults(data.results);
+        setSearchExpanded(true);
       } catch (err) {
         console.log(err);
       }
     }
   };
 
-  const addTaskIcon = (
-    <NavLink className={`${styles.CreateTaskButton}`} to="/tasks/create">
-      <i className="fa-solid fa-calendar-plus"></i>Create task
-    </NavLink>
-  );
+  const closeSearchResults = () => {
+    setSearchExpanded(false);
+  };
 
   const handleSignout = async () => {
     try {
@@ -51,6 +64,12 @@ const NavBar = () => {
     }
   };
 
+  const addTaskIcon = (
+    <NavLink className={`${styles.CreateTaskButton}`} to="/tasks/create">
+      <i className="fa-solid fa-calendar-plus"></i>Create task
+    </NavLink>
+  );
+
   const loggedInIcons = (
     <>
       <NavLink
@@ -60,7 +79,6 @@ const NavBar = () => {
       >
         <i className="fa-solid fa-list-check"></i>Tasks
       </NavLink>
-
       <NavLink className={styles.NavLink} to="/" onClick={handleSignout}>
         <i className="fa-solid fa-right-from-bracket"></i>Sign out
       </NavLink>
@@ -94,10 +112,11 @@ const NavBar = () => {
 
   return (
     <Navbar
-      expanded={expanded}
+      expanded={navbarExpanded}
       className={styles.NavBar}
       expand="md"
       fixed="top"
+      ref={navbarRef}
     >
       <Container>
         <NavLink to="/">
@@ -106,33 +125,35 @@ const NavBar = () => {
           </Navbar.Brand>
         </NavLink>
         <Navbar.Toggle
-          ref={ref}
-          onClick={() => setExpanded(!expanded)}
+          onClick={() => setNavbarExpanded(!navbarExpanded)}
           aria-controls="basic-navbar-nav"
         />
         <Navbar.Collapse id="basic-navbar-nav">
-          <div className={styles.SearchInputContainer}>
-            <Form inline>
+          <div className={styles.SearchInputContainer} ref={searchRef}>
+            <Form inline onSubmit={(e) => e.preventDefault()}>
               <FormControl
                 type="text"
                 placeholder="Search for users"
                 className="mr-sm-2"
                 value={searchQuery}
                 onChange={handleSearchChange}
-                onClick={(e) => e.key === "Enter" && handleSearchChange(e)}
               />
-              <Button variant="outline-success" onClick={handleSearchChange}>
-                Search
-              </Button>
             </Form>
-
-            {searchResults.length > 0 && (
+            {searchExpanded && (
               <div className={styles.SearchResultsDropdown}>
-                {searchResults.map((user) => (
-                  <NavLink key={user.id} to={`/accounts/${user.id}`}>
-                    {user.username}
-                  </NavLink>
-                ))}
+                {searchResults.length > 0 ? (
+                  searchResults.map((user) => (
+                    <NavLink
+                      key={user.id}
+                      to={`/accounts/${user.id}`}
+                      onClick={closeSearchResults}
+                    >
+                      {user.username}
+                    </NavLink>
+                  ))
+                ) : (
+                  <div className={styles.NoUserFound}>No user found</div>
+                )}
               </div>
             )}
           </div>
