49d315fa7bfbfdb4bb8537112768615ddd6bbea6
SoroushGReza <mycodeschool22@outlook.com>
2024-01-18 14:10:47 +0000

    Fix: Imported useEffect

--- frontend/src/pages/accounts/UserPasswordForm.js
+++ frontend/src/pages/accounts/UserPasswordForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Alert from "react-bootstrap/Alert";
 import Button from "react-bootstrap/Button";
