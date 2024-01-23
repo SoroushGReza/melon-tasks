43415dd79e591421eeb27f8e70ba04fe4352e1d7
SoroushGReza <mycodeschool22@outlook.com>
2024-01-23 09:28:05 +0000

    Fix: error 'line to long' to follow PEP8 guidelines

--- manage.py
+++ manage.py
@@ -6,7 +6,9 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_plans_drf_api.settings')
+    os.environ.setdefault(
+        'DJANGO_SETTINGS_MODULE', 'my_plans_drf_api.settings'
+    )
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
