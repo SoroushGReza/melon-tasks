902172d1e8161cd175e0366957ba0f90a25208b9
SoroushGReza <mycodeschool22@outlook.com>
2024-01-23 19:41:58 +0000

    Added: Comments for better readability

--- my_plans_drf_api/serializers.py
+++ my_plans_drf_api/serializers.py
@@ -2,10 +2,14 @@ from dj_rest_auth.serializers import UserDetailsSerializer
 from rest_framework import serializers
 
 
+# Serializer class for the current user
 class CurrentUserSerializer(UserDetailsSerializer):
+    # Create a read-only field for account_id
     account_id = serializers.ReadOnlyField(source='account.id')
+    # Create a read-only field for account_image
     account_image = serializers.ReadOnlyField(source='account.image.url')
 
+    # Meta class
     class Meta(UserDetailsSerializer.Meta):
         fields = UserDetailsSerializer.Meta.fields + (
             'account_id', 'account_image'
