2aed7a8507e1ea32663171e128750f18b0ce58b6
SoroushGReza <mycodeschool22@outlook.com>
2024-01-23 14:38:15 +0000

    Added: vomments for better readability, and fixed PEP warnings

--- my_plans_drf_api/permissions.py
+++ my_plans_drf_api/permissions.py
@@ -1,8 +1,11 @@
 from rest_framework import permissions
 
 
+# Check if a user is owner of object or if request is read-only
 class IsOwnerOrReadOnly(permissions.BasePermission):
+    # Override method to determine permission
     def has_object_permission(self, request, view, obj):
+        # Allow read-only methods for any user
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj.owner == request.user
--- my_plans_drf_api/serializers.py
+++ my_plans_drf_api/serializers.py
@@ -1,6 +1,7 @@
 from dj_rest_auth.serializers import UserDetailsSerializer
 from rest_framework import serializers
 
+
 class CurrentUserSerializer(UserDetailsSerializer):
     account_id = serializers.ReadOnlyField(source='account.id')
     account_image = serializers.ReadOnlyField(source='account.image.url')
