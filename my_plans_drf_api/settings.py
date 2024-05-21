0698efa4c89efc0918ee6901ea11d072012ccf1d
SoroushGReza <mycodeschool22@outlook.com>
2024-05-21 09:00:27 +0000

    collectstatic

--- my_plans_drf_api/settings.py
+++ my_plans_drf_api/settings.py
@@ -190,9 +190,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
+# WHITENOISE_ROOT = BASE_DIR / 'staticfiles' / 'build'
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'staticfiles' / 'build' / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 WHITENOISE_ROOT = BASE_DIR / 'staticfiles' / 'build'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
