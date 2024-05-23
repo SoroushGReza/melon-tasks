b6d9f3fe039c367eaa15c1084cf402b66403314c
SoroushGReza <mycodeschool22@outlook.com>
2024-05-23 17:07:32 +0000

    Fixed day cell height and added scroll for tasks overflow in MyCalendar component.

--- frontend/src/components/MyCalendar.js
+++ frontend/src/components/MyCalendar.js
@@ -68,6 +68,7 @@ const MyCalendar = ({ tasks }) => {
         }}
         events={calendarTasks}
         eventClick={handleEventClick}
+        dayCellClassNames={styles.dayCell}
       />
 
       <Modal
